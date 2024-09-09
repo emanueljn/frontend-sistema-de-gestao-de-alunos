d5beb5df994c4dd683592f81e947a3f048a9e2ee
Emanuel de Jesus Nardes <ejn.tech.innovation@gmail.com>
2024-09-09 20:29:57 -0300

    feat: Criar projeto Next.js

---
+++ sistema-de-gestao-de-alunos/src/app/dashboard/page.js
@@ -0,0 +1,19 @@
+export default function dashboard() {
+    return (
+      <>
+        <header>
+        
+        </header>
+
+        <main>
+            <div>
+                <h2>Alunos</h2>
+            </div>
+        </main>
+
+        <footer>
+            
+        </footer>
+      </>
+    )
+}
