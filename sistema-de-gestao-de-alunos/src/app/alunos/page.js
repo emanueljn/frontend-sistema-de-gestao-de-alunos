e24055c70eb610c90af1fe3f08eb485526caa1e0
Emanuel de Jesus Nardes <ejn.tech.innovation@gmail.com>
2024-09-09 22:20:27 -0300

    feat: adicionar html e css dos paineis de pesquisa de alunos e turmas

--- sistema-de-gestao-de-alunos/src/app/alunos/page.js
+++ sistema-de-gestao-de-alunos/src/app/alunos/page.js
@@ -1,3 +1,5 @@
+import styles from '../../styles/alunos.module.css'
+
 export default function alunos() {
     return (
       <>
@@ -5,8 +7,34 @@ export default function alunos() {
         
         </header>
 
-        <main className="conteudo__principal">
+        <main className={styles.conteudo__principal}>
+          <div className={styles.conteudo__principal__alunos}>
+            <h2 className={styles.conteudo__principal__alunos__subtitulo}>
+              Alunos
+            </h2>
+            <div className={styles.conteudo__principal__alunos__navegacao}>
+              <input className={styles.conteudo__principal__alunos__navegacao__entrada} placeholder='Pesquise o aluno por nome'></input>
+              <select className={styles.conteudo__principal__alunos__navegacao__filtro}></select>
+              <a><img src='./images/plus_icon.svg' className={styles.conteudo__principal__alunos__navegacao__imagem} alt='Ícone Adicionar'></img></a>
+            </div>
+            <div className={styles.conteudo__principal__alunos__resultado}>
 
+            </div>
+          </div>
+
+          <div className={styles.conteudo__principal__alunos}>
+            <h2 className={styles.conteudo__principal__alunos__subtitulo}>
+              Turmas
+            </h2>
+            <div className={styles.conteudo__principal__alunos__navegacao}>
+              <input className={styles.conteudo__principal__alunos__navegacao__entrada} placeholder='Pesquise o aluno por nome'></input>
+              <select className={styles.conteudo__principal__alunos__navegacao__filtro}></select>
+              <a><img src='./images/plus_icon.svg' className={styles.conteudo__principal__alunos__navegacao__imagem} alt='Ícone Adicionar'></img></a>
+            </div>
+            <div className={styles.conteudo__principal__alunos__resultado}>
+
+            </div>
+          </div>
         </main>
 
         <footer>
