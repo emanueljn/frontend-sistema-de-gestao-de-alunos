7777b12ee77e58c4e17b5eb18e6a9adafa7a377f
Emanuel de Jesus Nardes <ejn.tech.innovation@gmail.com>
2024-09-25 23:18:17 -0300

    fix: corrigir campos uf e complemento no cadastramento de alunos

--- sistema-de-gestao-de-alunos/src/app/cadastrarAluno/page.js
+++ sistema-de-gestao-de-alunos/src/app/cadastrarAluno/page.js
@@ -245,8 +245,8 @@ export default function Example() {
                     </label>
                     <div className="mt-2">
                         <input
-                            id="cidade"
-                            name="cidade"
+                            id="complemento"
+                            name="complemento"
                             type="text"
                             autoComplete="address-level1"
                             className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.cidade ? 'ring-red-500' : ''}`}
@@ -317,8 +317,8 @@ export default function Example() {
                     </label>
                     <div className="mt-2">
                         <input
-                            id="cidade"
-                            name="cidade"
+                            id="uf"
+                            name="uf"
                             type="text"
                             autoComplete="address-level1"
                             className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.cidade ? 'ring-red-500' : ''}`}
