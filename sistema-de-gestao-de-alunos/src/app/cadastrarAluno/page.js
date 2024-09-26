b8ef93a97c52f24ba6376fcae18e5fd4eea4793a
Emanuel de Jesus Nardes <ejn.tech.innovation@gmail.com>
2024-09-25 23:09:35 -0300

    fix: readicionar uf e complemento no cadastro do usuario

--- sistema-de-gestao-de-alunos/src/app/cadastrarAluno/page.js
+++ sistema-de-gestao-de-alunos/src/app/cadastrarAluno/page.js
@@ -241,6 +241,24 @@ export default function Example() {
                 </div>
 
                 <div className="sm:col-span-3">
+                    <label htmlFor="cidade" className="block text-sm font-medium leading-6 text-gray-900">
+                        Complemento
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            id="cidade"
+                            name="cidade"
+                            type="text"
+                            autoComplete="address-level1"
+                            className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.cidade ? 'ring-red-500' : ''}`}
+                            value={formData.complemento}
+                            onChange={handleChange}
+                        />
+                        {errors.complemento && <p className="mt-2 text-sm text-red-600">{errors.complemento}</p>}
+                    </div>
+                </div>
+
+                <div className="sm:col-span-3">
                     <label htmlFor="numero" className="block text-sm font-medium leading-6 text-gray-900">
                         Número
                     </label>
@@ -291,6 +309,24 @@ export default function Example() {
                             onChange={handleChange}
                         />
                         {errors.cidade && <p className="mt-2 text-sm text-red-600">{errors.cidade}</p>}
+                    </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                    <label htmlFor="cidade" className="block text-sm font-medium leading-6 text-gray-900">
+                        UF
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            id="cidade"
+                            name="cidade"
+                            type="text"
+                            autoComplete="address-level1"
+                            className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.cidade ? 'ring-red-500' : ''}`}
+                            value={formData.uf}
+                            onChange={handleChange}
+                        />
+                        {errors.uf && <p className="mt-2 text-sm text-red-600">{errors.uf}</p>}
                     </div>
                 </div>
 
